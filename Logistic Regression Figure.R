59d3108a3937a29308ae6cc387d8c48f1fd69d04
Shedd <kyle.shedd@dfg.alaska.local>
2017-02-13 15:28:08 -0900

    Create logistic summary regression figure for CRAA

--- Logistic Regression Figure.R
+++ Logistic Regression Figure.R
@@ -10,22 +10,30 @@ logistic=function(x,kappa,gamma){
   return(p)
 }
 
-
-par(mar=c(3.1, 4.6, 1.1, 1.1))
-par(family="serif")
-
 Birch.WLS.RegCoeffs=read.xlsx(file="Chignik inseason summary data.xlsx",sheetName="Birch.WLS.RegCoeffs",stringsAsFactors=FALSE,colClasses=c(rep("numeric",3)))
 Birch.WLS.RegCoeffs$col <- colorRampPalette(c("black", "skyblue"))(7)
 Birch.WLS.RegCoeffs$lty <- c(1:6, 1)
 
-plot(-5,cex=1.5,xlim=c(1,76),ylim=c(0,1),bty="n",axes=FALSE,cex.lab=2,xlab="",ylab="Proportion Black Lake",col.lab=1)
-mtext(text = "Date", side = 1, line = 2, cex = 2)
-rect(xleft=29,ybottom=0,xright=69,ytop=1,col="grey90")
-axis(side=1,at=seq(1,71,by=10),labels=c("5/25","6/4","6/14","6/24","7/4","7/14","7/24","8/3"),cex.axis=1.5,pos=0,lwd=4,col=1,col.axis=1)
-axis(side=2,cex.axis=1.5,lwd=4,col=1,col.axis=1)
+png(filename = "V:/Presentations/Regional/4_Westward/Sockeye/CRAA/2017/Logistic Regression Summary Plot.png", width = 7, height = 6.5, units = "in", res = 400)
+
+par(mar=c(3.1, 4.6, 1.1, 1.1))
+par(family="serif")
+par(bg=rgb(red=31,green=73,blue=125,maxColorValue=255))
+
+plot(-5,cex=1.5,xlim=c(1,76),ylim=c(0,1),bty="n",axes=FALSE,cex.lab=2,xlab="",ylab="",col.lab=1)
+mtext(text = "Date", side = 1, line = 2, cex = 2, col = "white")
+mtext(text = "Proportion Black Lake", side = 2, line = 3, cex = 2, col = "white")
+rect(xleft=29,ybottom=0,xright=69,ytop=1,col="white")
+axis(side=1,at=seq(1,71,by=10),labels=c("5/25","6/4","6/14","6/24","7/4","7/14","7/24","8/3"),cex.axis=1.5,pos=0,lwd=4,col="white",col.axis="white")
+axis(side=2,cex.axis=1.5,lwd=4,col="white",col.axis="white")
 apply(Birch.WLS.RegCoeffs, 1, function(yr) {
   lines(x = 1:76, y = 1-logistic(x = 1:76, kappa = as.numeric(yr[2]), gamma = as.numeric(yr[3])), type = "l", lwd = 3, col = yr[4], lty = as.numeric(yr[5]))
 } )
-abline(h=c(0),lwd=4,col=1)
-legend(x=0,y=1,bty="n",legend=2010:2016,lwd=4,col=Birch.WLS.RegCoeffs$col, lty = Birch.WLS.RegCoeffs$lty,cex=1.5,text.col=1)
+yr2 <- as.vector(Birch.WLS.RegCoeffs[7, ])
+lines(x = 1:76, y = 1-logistic(x = 1:76, kappa = as.numeric(yr2[2]), gamma = as.numeric(yr2[3])), type = "l", lwd = 10, col = as.character(yr2[4]), lty = as.numeric(yr2[5]))
+
+abline(h=c(0),lwd=4,col="white")
+legend(x=0,y=1,bty="n",legend=2010:2016,lwd=c(rep(4, 6), 10),col=Birch.WLS.RegCoeffs$col, lty = Birch.WLS.RegCoeffs$lty,cex=1.5,text.col="white")
+
+dev.off()
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
